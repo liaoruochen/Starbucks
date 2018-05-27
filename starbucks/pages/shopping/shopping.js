5639f2c201d5a394e054d813a5d616f647b7f56c
liaoruochen <920037556>
2018-05-27 10:01:13 +0800

    实现了页面之间数据的传递，并新增了点击商品图片，弹出商品信息框

--- starbucks/pages/shopping/shopping.js
+++ starbucks/pages/shopping/shopping.js
@@ -1,64 +1,151 @@
 // pages/shopping/shopping.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    titlePic:"/images/a.png",
+    titlePic:"",
     addNum:'/images/add.png',
     delNum:'/images/jian.png',
+    delNum1:'/images/jjjjj.png',
+    disabled:true,
+    money:0,
+    number:0,
     id:1,
+    status:0,
+    checkxx:true,
     goupic:'/images/gou.png'
     ,
-    listPic:[
-      {url:'/images/a.png',id:1,},
-      {url:'/images/b.png',id:2,},
-      {url:'/images/c.png',id:3,},
-      {url:'/images/d.png',id:4,},
-      {url:'/images/e.png',id:5,},
-      {url:'/images/f.png',id:6,},
-    ],
-    starbucksList:[
-      {url:'/images/g.png',name:"当季特饮",price:"40"},
-      {url:'/images/g.png',name:"拿铁",price:"31"},
-      {url:'/images/g.png',name:"冷萃冰咖啡",price:"36"},
-      {url:'/images/g.png',name:"星礼卡",price:"50"},
-      {url:'/images/g.png',name:"星礼卡",price:"100"},
-      {url:'/images/g.png',name:"馥芮白",price:"36"},
-      {url:'/images/g.png',name:"焦糖玛奇朵",price:"35"},
-      {url:'/images/g.png',name:"绵云冷萃",price:"42"},
-      {url:'/images/g.png',name:"星礼卡",price:"200"},
-      {url:'/images/g.png',name:"星礼卡",price:"288"},
-      {url:'/images/g.png',name:"星礼卡",price:"520"},
-     
-      
-    ],
+    listPic:[],
+    starbucksList:[],
+    introduce:[
+      {index:0,name:"当季特饮"}
+    ]
 
   },
   showChoose(e){
     // console.log(e)
     const id = e.currentTarget.dataset.id;
     const url = e.currentTarget.dataset.url;
-    console.log(e)
+    // console.log(e)
     this.setData({
       titlePic: url,
       id:id
-
     })
   },
+  addNumber(e){
+
+    let money = this.data.money;
+    
+
+    let number = this.data.number;
+    const index = e.currentTarget.dataset.index;
+    const status = e.currentTarget.dataset.status;
+    let price = Number(this.data.starbucksList[index].price);
+    let starbucksList = this.data.starbucksList;
+    let num = Number(this.data.starbucksList[index].num) ;
+    num++;
+    number++;
+    this.data.starbucksList[index].num=num
+    money+=price
+   
+    // console.log(this.data.starbucksList[index].num)
+    // starbucksList
+    
+    this.setData({
+      status,
+      starbucksList,
+      number,
+      money:money
+    })
+    this.hasNumber();
+    // console.log(starbucksList)
+  },
+  delNumber(e){
+    let money = this.data.money;
+    let number = this.data.number;
+    const index = e.currentTarget.dataset.index;
+    const status = e.currentTarget.dataset.status;
+    let price = this.data.starbucksList[index].price;
+    let starbucksList = this.data.starbucksList;
+    let num = this.data.starbucksList[index].num;
+    num--;
+    number--;
+    this.data.starbucksList[index].num=num
+    money= money-price
+    
+    // console.log(this.data.starbucksList[index].num)
+    // starbucksList  
+    this.setData({
+      status,
+      starbucksList,
+      number,
+      money:money
+
+    })
+    this.hasNumber();
+  },
+  hasNumber(){
+    const disabled = this.data.disabled;
+    let number = this.data.number
+    if(number===0){
+      this.setData({
+        disabled:true,
+        // addNum:'/images/add.png',
+      })
+    }else if(number!=0){
+      this.setData({
+        disabled:false,
+        // addNum:'/images/jjjjj.png',
+      })
+    }
+  },
+  buySomething(e){
+    wx.showToast({
+      title: '购买成功',
+      icon: 'success',
+      duration: 2000
+    })
+  },
+  checkDrink(e){
+    this.setData({
+      checkxx:false,
+    })
+  },
+  chaxx(e){
+    this.setData({
+      checkxx:true,
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    const page = options.page;
+    wx.request({
+      url:'https://www.easy-mock.com/mock/5adecfefc57e6f08ff16594b/example/starbucks',
+      success:(res)=>{
+        // console.log(res.data.data.listPic)
+        const listPic =  res.data.data.listPic
+        // console.log(listPic[page])
+        this.setData({
+          listPic:res.data.data.listPic[page].pagelist,
+          starbucksList:res.data.data.starbucksList,
+          titlePic:res.data.data.listPic[page].pagelist[0].url
+        })
+      }
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
@@ -101,5 +188,5 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
 })
