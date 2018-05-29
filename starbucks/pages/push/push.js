6e4d3a64c232e3bb48a2ca61fd6c9918eac63254
liaoruochen <920037556>
2018-05-29 08:26:05 +0800

    解决了一些样式上的bug 新增弹出层之后点击空白区域可以退出

--- starbucks/pages/push/push.js
+++ starbucks/pages/push/push.js
@@ -10,7 +10,15 @@ Page({
     addNum:'/images/add.png',
     delNum:'/images/jian.png',
     delNum1:'/images/jjjjj.png',
+    picimg:'',
+    thename:'',
+    themoney:'',
+    thetime:'',
+    themake:'',
+    checkxx:true,
     disabled:true,
+    scrolly:true,
+    scrollx:true,
     money:0,
     number:0,
     id:1,
@@ -20,6 +28,7 @@ Page({
     listPic:[],
     starbucksList:[
       {
+        imgnum:0,
         url: "/images/c1.png",
         name: "冰尚星冰粽",
         price: 198,
@@ -27,6 +36,7 @@ Page({
         num: 0
       },
       {
+        imgnum:1,
         url: "/images/c2.png",
         name: "冰逸星冰粽",
         price: 198,
@@ -34,6 +44,10 @@ Page({
         num: 0
       },
     ],
+    introduce:[
+      {thetime:'2018.05.27-2018.06.15',themake:"一本券为星冰粽冰尚款礼袋电子兑换券，凭券可兑领一份星冰粽冰尚款礼袋（含 10 颗星冰粽及 1 个冰尚礼袋）。请您在 2018 年 6 月 15 日前完成预约，并在预约的提领日期前往预约的星巴克门店完成领取；一预约方式和操作请点击本券的“预约领取门店”页面查询。请您至少在领取前提前 1 天进行预约（江浙沪地区需提前 2 天）。若您未在预约的提领日期前往预约门店领取产品，您需要重新预约；一本券为提领指定产品的唯一提领凭证，兑领时请向门店咖啡师出示本券。未进行预约的，请恕门店不接受提领产品；一本券的有效期为购买之日起至 2018 年 6 月 15 日，有效期届满后该券即失效，原购买人只能根据 《 星巴克用星说社交礼品平台须知 》 的规定申请退款;"},
+      {thetime:'2018.05.27-2018.06.15',themake:"一本券为星冰粽冰逸款礼袋电子兑换券，凭券可兑领一份星冰粽冰逸款礼袋（含 10 颗星冰粽及 1 个冰逸礼袋）。请您在 2018 年 6 月 15 日前完成预约，并在预约的提领日期前往预约的星巴克门店完成领取；一预约方式和操作请点击本券的“预约领取门店”页面查询。请您至少在领取前提前 1 天进行预约（江浙沪地区需提前 2 天）。若您未在预约的提领日期前往预约门店领取产品，您需要重新预约；一本券为提领指定产品的唯一提领凭证，兑领时请向门店咖啡师出示本券。未进行预约的，请恕门店不接受提领产品；一本券的有效期为购买之日起至 2018 年 6 月 15 日，有效期届满后该券即失效，原购买人只能根据 《 星巴克用星说社交礼品平台须知 》 的规定申请退款；"},
+    ]
 
   },
   showChoose(e){
@@ -121,6 +135,42 @@ Page({
       icon: 'success',
       duration: 2000
     })
+    setTimeout(function(){
+      wx.navigateTo({
+        url:"../history/history"
+      })
+    },1500)
+  },
+  checkDrink(e){
+    console.log(e)
+    let imgnum = e.currentTarget.dataset.imgnum
+    const introduce = this.data.introduce;
+  
+    // console.log(introduce)
+    this.setData({
+      checkxx:false,
+      thetime:introduce[imgnum].thetime,
+      themake:introduce[imgnum].themake ,
+      themoney:e.currentTarget.dataset.price,
+      picimg:e.currentTarget.dataset.url,
+      thename:e.currentTarget.dataset.name,
+      scrolly:false,
+      scrollx:false,
+
+   
+    })
+  },
+  chaxx(e){
+    this.setData({
+      checkxx:true,
+      picimg:'',
+      thename:'',
+      themoney:'',
+      thetime:'',
+      themake:'',
+      scrolly:true,
+      scrollx:true,
+    })
   },
 
   /**
@@ -132,7 +182,8 @@ Page({
     wx.request({
       url:'https://www.easy-mock.com/mock/5adecfefc57e6f08ff16594b/example/starbucks',
       success:(res)=>{
-        console.log(res.data.data.listPic[8])
+        // console.log(res.data.data.listPic[8])
+        
         
         this.setData({
           listPic:res.data.data.listPic[8].pagelist,
